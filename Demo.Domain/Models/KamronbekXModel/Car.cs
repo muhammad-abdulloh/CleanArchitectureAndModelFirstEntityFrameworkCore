8fc069daf4f2cdad8d920f4717aa9fc9b386a599
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-08 21:54:31 +0500

    model for relations

---
+++ Demo.Domain/Models/KamronbekXModel/Car.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Demo.Domain.Models.KamronbekXModel
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<PersonCars> CarPersons { get; set; }
+    }
+}
---
+++ Demo.Domain/Models/KamronbekXModel/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo.Domain.Models.KamronbekXModel
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<PersonCars> PersonCars { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
