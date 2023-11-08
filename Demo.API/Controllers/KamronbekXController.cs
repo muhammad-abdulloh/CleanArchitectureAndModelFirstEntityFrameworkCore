ad06c6b22a55671e4b54348dbb40ac4d131b6ee4
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-08 21:54:42 +0500

    create and get controller

--- Demo.API/Controllers/KamronbekXController.cs
+++ Demo.API/Controllers/KamronbekXController.cs
@@ -1,11 +1,9 @@
-﻿using Demo.Application.Repositories.KamronbekXRepositories;
-using Demo.Infrastructure.Services.KamronbekXServices;
-using Microsoft.AspNetCore.Http;
+﻿using Demo.Infrastructure.Services.KamronbekXServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class KamronbekXController : ControllerBase
     {
@@ -19,7 +17,37 @@ namespace Demo.API.Controllers
         public async ValueTask<IActionResult> GetAll()
         {
             var entities = await kamronbekService.GetAllAsync();
-            return Ok(entities);
+            return Ok();
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetPeopleAsync()
+        {
+            var people = await kamronbekService.GetPeopleAsync();
+            return Ok(people);
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreatePersonAsync(string name)
+        {
+            bool res = await kamronbekService.CreatePersonAsync(name);
+            return Ok(res);
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateCarAsync(string name)
+        {
+            bool res = await kamronbekService.CreateCarAsync(name);
+            return Ok(res);
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreatePersonCarsAsync(int pid, int cid)
+        {
+            bool res = await kamronbekService.CreatePersonCarsAsync(pid, cid);
+            return Ok(res);
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateOrderAsync(string order, int personId)
+        {
+            bool res = await kamronbekService.CreateOrderAsync(order, personId);
+            return Ok(res);
         }
     }
 }
