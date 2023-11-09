307cbdd4e77da4ba58af5cdf511b38f1dd2def76
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-09 20:13:19 +0500

    personservice added

---
+++ Demo.Infrastructure/Services/PersonServices/IPersonService.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo.Infrastructure.Services.PersonServices;
+
+public interface IPersonService
+{
+    ValueTask<Person> GetPersonByIdAsync(int id);
+    ValueTask<IEnumerable<Person>> GetAllPersonsAsync();
+    ValueTask<bool> DeletePersonAsync(int id);
+    ValueTask<bool> UpdatePersonAsync(int id, string name);
+    ValueTask<bool> CreatePersonAsync(string personName);
+}
---
+++ Demo.Infrastructure/Services/PersonServices/PersonService.cs
@@ -0,0 +1,40 @@
+﻿using Demo.Application.Repositories.PersonRepositories;
+
+namespace Demo.Infrastructure.Services.PersonServices;
+
+public class PersonService : IPersonService
+{
+    private readonly IPersonRepository _personRepository;
+
+    public PersonService(IPersonRepository personRepository)
+    {
+        _personRepository = personRepository;
+    }
+
+    public async ValueTask<bool> CreatePersonAsync(string name)
+    {
+        var person = new Person { Name = name };
+        return await _personRepository.CreateAsync(person);
+    }
+
+    public async ValueTask<bool> DeletePersonAsync(int id)
+    {
+        return await _personRepository.DeleteAsync(id);
+    }
+
+    public async ValueTask<IEnumerable<Person>> GetAllPersonsAsync()
+    {
+        return await _personRepository.GetAllPersonAsync();
+    }
+
+    public async ValueTask<Person> GetPersonByIdAsync(int id)
+    {
+        return await _personRepository.GetPersonByIdAsync(id);
+    }
+
+    public async ValueTask<bool> UpdatePersonAsync(int id, string name)
+    {
+        var person = new Person { Id = id, Name = name };
+        return await _personRepository.UpdateAsync(person);
+    }
+}
