e48eb95558933f914bd3380bcaf09f4a5745f41f
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-09 20:02:25 +0500

    orders controller added

---
+++ Demo.API/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿global using Demo.Infrastructure.Services.OrdersService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.API.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class OrderController : ControllerBase
+{
+    private readonly IOrderService _orderService;
+
+    public OrderController(IOrderService orderRepository)
+    {
+        _orderService = orderRepository;
+    }
+    [HttpGet]
+    public async ValueTask<IActionResult> GetOrdersAsync()
+    {
+        var res = await _orderService.GetOrdersAsync();
+        return Ok(res);
+    }
+    [HttpGet]
+    public async ValueTask<IActionResult> GetOrderByIdAsync(int id)
+    {
+        var res = await _orderService.GetOrderByIdAsync(id);
+        return Ok(res);
+    }
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateOrder(int personId, string orderName)
+    {
+        var res = await _orderService.CreateOrderAsync(personId, orderName);
+    }
+}
