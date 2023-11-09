42923aa75016589882644c5c73a25b52f458a906
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-09 20:12:31 +0500

    person repository added

---
+++ Demo.Application/Repositories/PersonRepositories/IPersonRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo.Application.Repositories.PersonRepositories;
+
+public interface IPersonRepository
+{
+    public ValueTask<IEnumerable<Person>> GetAllPersonAsync();
+    public ValueTask<Person> GetPersonByIdAsync(int id);
+    public ValueTask<bool> UpdateAsync(Person person);
+    public ValueTask<bool> DeleteAsync(int id);
+    public ValueTask<bool> CreateAsync(Person person);
+    //public ValueTask<bool> AddOrdertoPerson(int personId, Order order);
+}
---
+++ Demo.Application/Repositories/PersonRepositories/PersonRepository.cs
@@ -0,0 +1,50 @@
+﻿namespace Demo.Application.Repositories.PersonRepositories;
+
+public class PersonRepository : IPersonRepository
+{
+    private readonly DemoDbContext _dbContext;
+
+    public PersonRepository(DemoDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async ValueTask<bool> CreateAsync(Person person)
+    {
+        await _dbContext.AddAsync(person);
+        int res = await _dbContext.SaveChangesAsync();
+        return res > 0;
+    }
+
+    public async ValueTask<bool> DeleteAsync(int id)
+    {
+        var person = await _dbContext.Persons.FirstOrDefaultAsync(x => x.Id == id);
+        if (person is null)
+        {
+            return false;
+        }
+        int res = await _dbContext.SaveChangesAsync();
+        return res > 0;
+    }
+
+    public async ValueTask<IEnumerable<Person>> GetAllPersonAsync()
+    {
+        return await _dbContext.Persons.ToListAsync();
+    }
+
+    public async ValueTask<Person> GetPersonByIdAsync(int id)
+    {
+        var person = await _dbContext.Persons.FirstOrDefaultAsync(x => x.Id == id);
+        return person;
+    }
+
+    public async ValueTask<bool> UpdateAsync(Person person)
+    {
+        var tempperson = await _dbContext.Persons.FirstOrDefaultAsync(x => x.Id == person.Id);
+        if (tempperson is null)
+            return false;
+        tempperson.Name = person.Name;
+        int res = await _dbContext.SaveChangesAsync();
+        return (res > 0);
+    }
+}
