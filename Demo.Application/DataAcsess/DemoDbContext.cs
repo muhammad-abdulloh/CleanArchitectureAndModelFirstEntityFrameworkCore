534cb19a5f2d197c68c9671b64888ecca6b73a67
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-11 21:06:12 +0500

    added modelbuilder

--- Demo.Application/DataAcsess/DemoDbContext.cs
+++ Demo.Application/DataAcsess/DemoDbContext.cs
@@ -1,6 +1,8 @@
-﻿using Demo.Domain.Models;
+﻿using Demo.Application.Configurations;
+using Demo.Domain.Models;
 using Demo.Domain.Models.KamronbekXModel;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Demo.Application.DataAcsess
 {
@@ -9,11 +11,22 @@ namespace Demo.Application.DataAcsess
         public DemoDbContext(DbContextOptions<DemoDbContext> options)
             : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetCallingAssembly());
+
+            //alohida berish ham mumkin
+            //modelBuilder.ApplyConfiguration(new CarTypeConfiguration());
+        }
+       
         public DbSet<User> Users { get; set; }
         public DbSet<KamronbekX> KamronbekXes { get; set; }
         public DbSet<Person> Persons { get; set; }
         public DbSet<Car> Cars { get; set; }
         public DbSet<PersonCars> PersonCars { get; set; }
         public DbSet<Order> Orders { get; set; }
+
+
     }
 }
