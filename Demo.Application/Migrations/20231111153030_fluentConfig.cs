9cecb798c7ec00ba5dc911918963c40619d9bfeb
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-11 20:55:30 +0500

    added migrations

---
+++ Demo.Application/Migrations/20231111153030_fluentConfig.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class fluentConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Persons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Persons",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Cars",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Cars");
+        }
+    }
+}
